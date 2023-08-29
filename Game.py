acc829b2e50252b796b817ecaa0b9b2ff2c18d39
Amit Tambulkar <amittambulkar104@gmail.com>
2023-08-29 20:15:32 +0530

    Updated

--- Game.py
+++ Game.py
@@ -6,6 +6,12 @@ class TicTackToe:
     def print_board(self):
         for row in[self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
+    
+    def empyt_squares(self):
+        return ' ' in self.board
+    
+    def num_empty_squares(self):
+        return self.board.count(' ')
 
     @staticmethod
     def print_board_nums():
@@ -15,3 +21,20 @@ class TicTackToe:
 
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == ' ']
+    
+def play(game, x_player, o_player, print_game=True)
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X' # starting letter
+    # itterate while game still have empty squares
+    # No need to worry about winner we will just return it which will break the loop
+
+    while game.empty_squares():
+        # get the move from appropriate player
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+    
+    # let's define a function to make a move
--- Player.py
+++ Player.py
@@ -15,11 +15,28 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
     
     def get_move(self, game):
-        pass
+        # get a random valid spot for the next move of computer
+        square = random.choice(game.available_move())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
     
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # We will check whether the values is correct or not.
+            # If it is not then will return Invalid input
+            # If the spot is not available then will return Invalid input
+            try:
+                val = int(square)
+                if val not in game.available_moves:
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again. ")
+        return val
+
